e59cb4e56b9e17374b4dfb063843bee06a15c249
Chulaluck-57160284 <57160284@go.buu.ac.th>
2016-10-08 15:56:41 +0700

    update main

--- main.rb
+++ main.rb
@@ -12,7 +12,32 @@ else
 end
      puts a
 end  
-  
+
+puts "ข้อ 2"
+
+def prime_fib (number)
+    
+   fibo = "{" 
+    #puts do_hash
+    nub1 = 1
+    until nub1 > number
+      fibonacci = Hash.new{ |h,k| h[k] = k < 2 ? k : h[k-1] + h[k-2] }
+    fiboanwer = fibonacci[nub1]
+    
+    ff = "f" << nub1
+    
+    fibo << "f#{nub1} => #{fiboanwer} , "
+    
+    #puts nub1
+    nub1 = nub1 + 1
+end
+sub_s = fibo.length-2
+    hash_return = fibo[0...sub_s]
+    hash_return << "}"
+    return hash_return
+end
+puts prime_fib (10)
+
 puts "ข้อ 4"
 US_states.each do |k,v|
 	    text_rut = "#{k}".upcase
